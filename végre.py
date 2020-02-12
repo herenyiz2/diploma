f683f8ea0fe470ec0c1e2edb9f0c00bdab6693ae
herenyiz2 <60981234+herenyiz2@users.noreply.github.com>
2020-02-12 18:30:18 +0100

    Create végre.py

---
+++ végre.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: %(username)s
+"""
+
+#1 szám kiiratása
+print (5)
+
+#s müvelet eredményének kiirása
+print (5+4)
+
+# 'n' változóba eltarolunk egy szöveget, majd kiirni
+n = "hellllo"
+print (n)
+
+#
